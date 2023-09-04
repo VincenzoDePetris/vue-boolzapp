eb87d5617d823377cafc853d354bc7c496d578e6
VincenzoDePetris <vince02@live.it>
2023-09-04 12:36:50 +0200

    add: milestone 3 + controllo messaggi vuoti

--- js/main.js
+++ js/main.js
@@ -5,11 +5,32 @@ createApp({
     return {
       contacts,
       current: 0,
+      newMessage: {
+        date: "",
+        message: "",
+        status: "sent",
+      },
+      newResent: {
+        date: "",
+        message: "Ok",
+        status: "received",
+      },
     };
   },
   methods: {
     setContact(index) {
       this.current = index;
+    },
+    newCopyMessage() {
+      const newMessage2 = { ...this.newMessage };
+      if (newMessage2.message.length == 0) {
+      } else {
+        this.contacts[this.current].messages.push(newMessage2);
+        setTimeout(() => {
+          this.contacts[this.current].messages.push(this.newResent);
+        }, 1000);
+      }
+      this.newMessage.message = "";
     },
   },
 }).mount("#app");
